5e292f06c24ec7c06954a355cffa8fb5842dcbf0
Schmid Simon <schms27@gmail.com>
2023-09-06 17:24:59 +0200

    add .net core web api example

---
+++ c#/api/Mongodb_Rest_API/Mongodb_Rest_API/Models/DatabaseSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace Mongodb_Rest_API.Models
+{
+    public class DatabaseSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+
+        public string DatabaseName { get; set; } = null!;
+
+        public string BooksCollectionName { get; set; } = null!;
+    }
+}
